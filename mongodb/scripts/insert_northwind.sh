1860b77ce0caceb985a776a76472c58ae2bf77bf
Jennifer Reif <jennifer.reif@neo4j.com>
2021-08-19 16:28:08 -0500

    Fix quoting issue in mongo load script

--- mongodb/scripts/insert_northwind.sh
+++ mongodb/scripts/insert_northwind.sh
@@ -16,7 +16,7 @@ do
     mgrFName=$(mongo $auth $db --eval 'db.employee.findOne({employeeId:'$reportsTo'},{_id:0,firstName:1}).firstName;' --quiet)
     mgrLName=$(mongo $auth $db --eval 'db.employee.findOne({employeeId:'$reportsTo'},{_id:0,lastName:1}).lastName;' --quiet)
     #echo "$employeeId | $mgrId | $mgrFName | $mgrLName"
-    mongo $auth $db --eval 'db.employee.update({employeeId:'$employeeId'}, {$set:{ReportsTo:{managerId:'$mgrId',mgrFirstName:"'$mgrFName'",mgrLastName:"'$mgrLName'"}}});' >> employee_reporting.log
+    mongo $auth $db --eval 'db.employee.update({employeeId:'$employeeId'}, {$set:{ReportsTo:{managerId:'$mgrId',mgrFirstName:"'"$mgrFName"'",mgrLastName:"'"$mgrLName"'"}}});' >> employee_reporting.log
   fi
 done
 
@@ -28,15 +28,16 @@ sed 1d order_details.csv | while IFS="," read orderId productId unitPrice quanti
 do
   #echo "$orderId | $productId | $unitPrice | $quantityOrdered | $discount"
   productName=$(mongo $auth $db --eval 'db.product.findOne({productId:'$productId'},{_id:0,productName:1}).productName;' --quiet)
-  mongo $auth $db --eval 'db.order.update({orderId:'$orderId'}, {$addToSet:{Products:{productId:'$productId',productName:"'$productName'",unitPrice:'$unitPrice',quantityOrdered:'$quantityOrdered',discount:'$discount'}}});' >> order_details.log
+  #echo "$productId | $productName"
+  mongo $auth $db --eval 'db.order.update({orderId:'$orderId'}, {$addToSet:{Products:{productId:'$productId',productName:"'"$productName"'",unitPrice:'$unitPrice',quantityOrdered:'$quantityOrdered',discount:'$discount'}}});' >> order_details.log
 done
 
 sed 1d order_contacts.csv | while IFS="," read orderId customerId employeeId
 do
   #echo "$orderId | $customerId | $employeeId"
-  companyName=$(mongo $auth $db --eval 'db.customer.findOne({customerId:"'$customerId'"},{_id:0,companyName:1}).companyName;' --quiet)
+  companyName=$(mongo $auth $db --eval 'db.customer.findOne({customerId:"'"$customerId"'"},{_id:0,companyName:1}).companyName;' --quiet)
   employeeFName=$(mongo $auth $db --eval 'db.employee.findOne({employeeId:'$employeeId'},{_id:0,firstName:1}).firstName;' --quiet)
   employeeLName=$(mongo $auth $db --eval 'db.employee.findOne({employeeId:'$employeeId'},{_id:0,lastName:1}).lastName;' --quiet)
-  mongo $auth $db --eval 'db.order.update({orderId:'$orderId'}, {$set:{Customer:{customerId:"'$customerId'",companyName:"'$companyName'"}}});' >> order_contacts.log
-  mongo $auth $db --eval 'db.order.update({orderId:'$orderId'}, {$set:{Employee:{employeeId:'$employeeId',firstName:"'$employeeFName'",lastName:"'$employeeLName'"}}});' >> order_contacts.log
+  mongo $auth $db --eval 'db.order.update({orderId:'$orderId'}, {$set:{Customer:{customerId:"'"$customerId"'",companyName:"'"$companyName"'"}}});' >> order_contacts.log
+  mongo $auth $db --eval 'db.order.update({orderId:'$orderId'}, {$set:{Employee:{employeeId:'$employeeId',firstName:"'"$employeeFName"'",lastName:"'"$employeeLName"'"}}});' >> order_contacts.log
 done
